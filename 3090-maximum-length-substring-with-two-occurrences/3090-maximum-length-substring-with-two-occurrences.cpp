14c495550eb9516fbf2ec6742ef840cd37ecea3f
KARAN SINGH <karansingh102005@gmail.com>
2026-08-14 11:11:01 +0530

    Time: 3 ms (42.93%), Space: 9.5 MB (57.72%) - LeetHub

---
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        int n = s.length();
+        int ans = 0;
+
+        unordered_map<char, int> mp;
+
+        int left = 0;
+
+        for (int right = 0; right < n; right++) {
+
+            mp[s[right]]++;
+
+            while (mp[s[right]] > 2) {
+                mp[s[left]]--;
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
